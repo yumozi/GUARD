206aaf388f1884cdb769a5520d142973fde4d2c7
william <liyijiang3000@gmail.com>
2025-06-17 05:59:57 -0700

    add website

--- pretrain/pretrain.sh
+++
@@ -1,9 +0,0 @@
-python pretrain.py \
---dataset imagenette \
---model resnet18 \
---opt sgd --lr 0.025 \
--wd 1e-4 -b 32 --lr-scheduler cosine --epochs 50 \
---augmix-severity 0 \
---ra-magnitude 0 \
---data-path /media/techt/DATA/data/imagenette
-
